c472f7899e36fbf445c7bd40ccfdb6854a559401
Famizu <sufozevu@oranek.com>
2018-10-21 18:50:41 +0100

    first commit

---
+++ Greeting.js
@@ -0,0 +1,36 @@
+/**
+ * A bot that welcomes new guild members when they join
+ */
+
+// Import the discord.js module
+const Discord = require('discord.js');
+
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find(ch => ch.name === 'recruitment');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server ${member},
+ Please type your ign and realmeye link and then 
+ use !accept to confirm you're account :D`);
+});
+
+
+//client.login('process.env.TOKEN');
+client.login('NTAyNTQwNzU1ODIzMjMwOTg3.Dq4P_g.WoNzOK8wOB0ewP0yQbK5GbpSmw0');
+require('http').createServer().listen(3000);
+//now --public -e token="NTAyNTQwNzU1ODIzMjMwOTg3.Dq4P_g.WoNzOK8wOB0ewP0yQbK5GbpSmw0"
+//now scale https://greeter-bot-koffuxvecp.now.sh sfo1 1
